f01d949bf26d94672d36bc5ea215e2f16ff27466
Student <student@bigbrainlinuxuser.jcv8387.com>
2020-09-11 20:19:49 -0400

    comments

--- script1.sh
+++ script1.sh
@@ -1,6 +1,12 @@
+#script1.sh checks what the default gateway is and uses ping check your connection to the default gatway, a remote server and the dns server.
+
+#get gateway from ip route command
 GATEWAY=$(ip route | awk '/default/ { print $3 }')
+#pings the gatewy and store successful into the variable if ping works
 GATEWAYCON=$(ping -c 1 $GATEWAY > /dev/null && echo "\e[42mSUCCESSFUL" || echo "\e[41mUNSUCCESSFUL")
+#pings google's server and store successful if ping works
 REMOTECON=$(ping -c 1 8.8.8.8 > /dev/null && echo "\e[42mSUCCEDED" || echo "\e[41mFAILED")
+#pings google's server by its dns name and store successful if ping works
 DNSCON=$(ping -c 1 www.google.com > /dev/null && echo "\e[42mSUCCEDED" || echo "\e[41mFAILED")
 clear
 echo "-----------------------------------------------"
