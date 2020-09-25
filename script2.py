7879ab730f4eb7e3a89b61608de3b6f80465194b
root <root@bigbrainlinuxuser.jcv8387.com>
2020-09-25 19:22:10 -0400

    finished finished

--- script2.py
+++ script2.py
@@ -5,7 +5,7 @@ import time
 for csv in [x for x in sys.argv[1:] if x[-4:] == ".csv"]:
     users = []
     for line in open(csv, "r").readlines():
-        eid, lname, fname, off, phone, dept, grp = line.split(",")
+        eid, lname, fname, off, phone, dept, grp = line.split(",")[:7]
         shell = "/bin/chs" if grp == "office" or grp == "ceo" else "/bin/bash"
         if eid == "EmployeeID":
             continue
@@ -22,18 +22,19 @@ for csv in [x for x in sys.argv[1:] if x[-4:] == ".csv"]:
                 .stdout.split(":")[0]:
             print(f"groupadd -f {grp}")
             subprocess.run(f"getent group {grp}".split(" "))
-        user = fname[:1] + lname
+        user = fname[:1] + lname.replace("'", "")
+        user = user.lower()
         dupe = str(users.count(user)) if users.count(user) > 0 else ""
         users.append(user)
         user = user + dupe
-        subprocess.run(f"useradd -m -d /home/{grp}/{user} -s {shell} -g {grp} -c \"{fname} {lname}\" {user}".split(" "))
+        subprocess.run(["useradd", "-m", "-d", f"/home/{grp}/{user}", "-s", shell, "-g", grp, "-c", f"\"{fname} {lname}\"", user])
         print(f"useradd -m -d /home/{grp}/{user} -s {shell} -g {grp} -c \"{fname} {lname}\" {user}")
         # TODO run passwd with stdin
-        p = subprocess.Popen(f"passwd {user}".split(" "), stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+        p = subprocess.Popen(f"passwd --stdin {user}".split(" "), stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
-        p.stdin.write(user[::-1])
+        p.stdin.write(user[::-1].encode())
         p.stdin.flush()
         print(p.stdout)
         time.sleep(.001)
-        e = subprocess.run(f"passwd -e {user}", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        e = subprocess.run(f"passwd -e {user}".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(e.stdout)
