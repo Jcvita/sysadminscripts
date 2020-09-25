07b9ef4b0025c017a833b389b8de095ad0b818e8
root <root@bigbrainlinuxuser.jcv8387.com>
2020-09-25 00:13:10 -0400

    sh so far 2

--- script2.sh
+++ script2.sh
@@ -1,20 +1,31 @@
-
 for csv in "$@"
 do
 
 	if [ -r $csv ]; then
-		while IFS=, read -r eid lname fname off phone dept grp ovrflow
+		while IFS=, read -r eid lname fname off phone dept grp
 		do
-			if [ $eid == "EmployeeID" ]; then
+			if [ $eid == "EmployeeID" ]; then continue; fi
+			user="${fname:0:1}$lname"
+			user="${user,,}" 
+			
+			if [[ $lname =~ [0-9] || $fname =~ [0-9] || $phone =~ [a-zA-Z] ]]; then
+				echo "BAD RECORD: $eid"
 				continue
 			fi
-			if [ ! $ovrflow == "" ]; then
-				continue 
-			getent group $grp || echo "groupadd -f $grp"
-			getent group $grp || groupadd -f $grp
+			if [ -z $eid || -z $lname || -z $fname || -z $off || -z $phone || -z $dept || -z $grp ]; then
+				echo "User account $user not added due to missing information."
+				continue
+			fi
+			if [[ ! "$(getent group $grp)" == *"$grp"* ]]; then
+				echo "groupadd -f $grp"
+				groupadd -f $grp
+			fi	
+			if [[ "$grp" == "office" || "$grp" == "ceo" ]]; then
+				shell=$("/bin/csh")
+			else
+				shell=$("/bin/bash")
+			fi
 			
-			
-			echo "$ovrflow"
 		done < $csv
 	
 	else
