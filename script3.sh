f5596cfdabfbd9b61704c8d5e3a1c3054d11f477
Jcvita <jcvita2012@gmail.com>
2020-10-09 03:26:39 -0400

    script3.sh

--- script3.sh
+++ script3.sh
@@ -3,8 +3,9 @@ echo "Your current working directory is $(pwd)"
 if [[ $(pwd) != "/home/$(whoami)/Desktop" ]]; then
 	echo -e "\e[41mWARNING\e[49m: your current working directory is not the desktop"
 fi
+echo "Enter a file for the user to create a symlink for. (quit to quit)" 
 read input
-while [input != "quit"]; do
+while [input != "quit" && find -type f ]; do
 	
 
 done
