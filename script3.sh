5d3c35aeb8c35f4753886eeed064180baf0d3910
root <root@bigbrainlinuxuser.jcv8387.com>
2020-10-09 20:33:15 -0400

    yeet

--- script3.sh
+++ script3.sh
@@ -5,7 +5,18 @@ if [[ $(pwd) != "/home/$(whoami)/Desktop" ]]; then
 fi
 echo "Enter a file for the user to create a symlink for. (quit to quit)" 
 read input
-while [input != "quit" && find -type f ]; do
-	
-
+OUTPUT=$(find / -name $input -type f | head -n 1)
+while [$input != "quit" && echo $OUTPUT | grep "Permission denied" ]; do
+	echo "file doesn't exist"
+	read input
 done
+if [input != "quit"]; then
+	exit 0
+fi
+FNAME = $(echo $OUTPUT | tr "/" "\n")
+ln -s $OUTPUT ${FNAME[-1]}
+NUM_LINKS = $(ls -la /home/$(whoami)/desktop | grep "\->")
+echo "Number of symlinks on the desktop: $NUM_LINKS"
+echo 
+
+
